e32e58fda2dfbf0c041babe1b29203f0769b1fab
Ahsan <ahsan.ejaz@dcmail.ca>
2024-07-30 10:15:52 -0400

    fixed bug in the evaluate-model.py

--- evaluate-model.py
+++ evaluate-model.py
@@ -1,16 +1,34 @@
 import joblib
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
 
-# Load the model
+# Load the model and vectorizer
 model = joblib.load('logreg_model.pkl')
+vect = joblib.load('vectorizer.pkl')
 
 # Load test data
-tweet_df = pd.read_csv('test.csv')
+tweet_df = pd.read_csv('train.csv')
 X_test = tweet_df['tweet']
 y_test = tweet_df['label']
 
-# Transform data and predict
+# Process the data similarly as in training
+def data_processing(tweet):
+    tweet = tweet.lower()
+    tweet = re.sub(r"https\S+|www\S+http\S+", '', tweet, flags = re.MULTILINE)
+    tweet = re.sub(r'\@w+|\#','', tweet)
+    tweet = re.sub(r'[^\w\s]','',tweet)
+    tweet = re.sub(r'ð','',tweet)
+    tweet_tokens = word_tokenize(tweet)
+    filtered_tweets = [w for w in tweet_tokens if not w in stop_words]
+    return " ".join(filtered_tweets)
+
+tweet_df.tweet = tweet_df['tweet'].apply(data_processing)
+
+# Transform test data using the loaded vectorizer
 X_test_transformed = vect.transform(X_test)
 predictions = model.predict(X_test_transformed)
 
